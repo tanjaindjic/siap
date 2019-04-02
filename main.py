a099a36f3e772f916e4710e86c8877bec6cdb2cf
Nina Miladinovic <ninamiladinovic@hotmail.com>
2019-04-02 18:14:57 +0200

    ubacivanje podataka u csv fajlove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from sklearn.metrics import jaccard_similarity_score
 import formData
-
+import pandas as pd
+import numpy as np
+import csv
 from vino import Vino
 import nltk
 from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
@@ -47,25 +49,25 @@ def nadjiTitlove():
     for ds in dataSetic:
         print(ds['title'])
 def loadTreningSet():
-    treningSet = formData.convertFromJson("validacioni")
-    # for v in data:
-    #     treningSet.append(Vino.initialize(Vino(), v['country'], v['description'], v['points'], v['price'], v['province'],
-    #                             v['taster_name'], v['title'], v['variety'], v['winery'], 0))
+    data = formData.convertFromJson("trening")
+    for v in data:
+        treningSet.append(Vino.initialize(Vino(), v['country'], v['description'], v['points'], v['price'], v['province'],
+                                v['taster_name'], v['title'], v['variety'], v['winery'], 0))
 def processTreningSet():
     # print("length: "+str(len(treningSet)))
     for v in treningSet:
-        if v['country'] not in countries:
-            countries.append(v['country'])
-        if v['province'] not in provinces:
-            provinces.append(v['province'])
-        if v['variety'] not in varieties:
-            varieties.append(v['variety'])
-        if v['winery'] not in wineries:
-            wineries.append(v['winery'])
-        if v['taster_name'] not in taster_names:
-            taster_names.append(v['taster_name'])
-        if v['title'] not in titles:
-            titles.append(v['title'])
+        if v.__getattribute__('country') not in countries:
+            countries.append(v.__getattribute__('country'))
+        if v.__getattribute__('province') not in provinces:
+            provinces.append(v.__getattribute__('province'))
+        if v.__getattribute__('variety') not in varieties:
+            varieties.append(v.__getattribute__('variety'))
+        if v.__getattribute__('winery') not in wineries:
+            wineries.append(v.__getattribute__('winery'))
+        if v.__getattribute__('taster_name') not in taster_names:
+            taster_names.append(v.__getattribute__('taster_name'))
+        if v.__getattribute__('title') not in titles:
+            titles.append(v.__getattribute__('title'))
             #print(v['title'])
     # print("countries: "+str(len(countries)))
     # print("provinces: "+str(len(provinces)))
@@ -80,47 +82,45 @@ def processTreningSet():
     len_taster_names = len(taster_names)
     len_title = len(titles)
     for v in treningSet:
-        country_index = countries.index(v['country'])+1
-        value_country = (1/len_countries)*country_index
-        v['country'] =  value_country
+        country_index = countries.index(v.__getattribute__('country')) + 1
+        value_country = (1 / len_countries) * country_index
+        v.__setattr__('country', value_country)
 
+        province_index = provinces.index(v.__getattribute__('province')) + 1
+        value_province = (1 / len_provinces) * province_index
+        v.__setattr__('province', value_province)
 
-        province_index = provinces.index(v['province'])+1
-        value_province = (1/len_provinces)*province_index
-        v['province'] =  value_province
-
-        variety_index = varieties.index(v['variety']) + 1
+        variety_index = varieties.index(v.__getattribute__('variety')) + 1
         value_variety = (1 / len_varieties) * variety_index
-        v['variety'] =  value_variety
+        v.__setattr__('variety', value_variety)
 
-
-        winery_index = wineries.index(v['winery']) + 1
+        winery_index = wineries.index(v.__getattribute__('winery')) + 1
         value_winery = (1 / len_wineries) * winery_index
-        v['winery'] =  value_winery
+        v.__setattr__('winery', value_winery)
 
-        taster_name_index = taster_names.index(v['taster_name']) + 1
+        taster_name_index = taster_names.index(v.__getattribute__('taster_name')) + 1
         value_taster_name = (1 / len_taster_names) * taster_name_index
-        v['taster_name'] =  value_taster_name
+        v.__setattr__('taster_name', value_taster_name)
 
-        title_index = titles.index(v['title']) + 1
+        title_index = titles.index(v.__getattribute__('title')) + 1
         value_title = (1 / len_title) * title_index
-        v['title'] =  value_title
+        v.__setattr__('title', value_title)
 
-        if(int(v['points'])<85):
-           v['points'] =  0
-        elif(int(v['points'])<90):
-           v['points'] =  0.33
-        elif(int(v['points'])<95):
-           v['points'] =  0.67
+        if(int(v.__getattribute__('points'))<85):
+           v.__setattr__('points', 0)
+        elif(int(v.__getattribute__('points'))<90):
+           v.__setattr__('points', 0.33)
+        elif(int(v.__getattribute__('points'))<95):
+           v.__setattr__('points', 0.67)
         else:
-           v['points'] =  1
+           v.__setattr__('points', 1)
 
 def loadTestSet():
-    testSet = formData.convertFromJson("test")
-    # for v in data:
-    #     testSet.append(
-    #         Vino.initialize(Vino(), v['country'], v['description'], v['points'], v['price'], v['province'],
-    #                         v['taster_name'], v['title'], v['variety'], v['winery'], 0))
+    data = formData.convertFromJson("test")
+    for v in data:
+        testSet.append(
+            Vino.initialize(Vino(), v['country'], v['description'], v['points'], v['price'], v['province'],
+                            v['taster_name'], v['title'], v['variety'], v['winery'], 0))
 def processTestSet():
     len_countries = len(countries)
     len_provinces = len(provinces)
@@ -129,56 +129,57 @@ def processTestSet():
     len_taster_names = len(taster_names)
     len_title = len(titles)
     for v in testSet:
-        if v['country'] not in countries:
-            v['country'] = 0
+        if v.__getattribute__('country') not in countries:
+            v.__setattr__('country', 0)
         else:
-            country_index = countries.index(v['country'])+1
-            value_country = (1/len_countries)*country_index
-            v['country'] = value_country
+            country_index = countries.index(v.__getattribute__('country')) + 1
+            value_country = (1 / len_countries) * country_index
+            v.__setattr__('country', value_country)
 
-        if v['province'] not in provinces:
-           v['province'] = 0
+        if v.__getattribute__('province') not in provinces:
+           v.__setattr__('province', 0)
         else:
-            province_index = provinces.index(v['province'])+1
-            value_province = (1/len_provinces)*province_index
-            v['province'] = value_province
+            province_index = provinces.index(v.__getattribute__('province')) + 1
+            value_province = (1 / len_provinces) * province_index
+            v.__setattr__('province', value_province)
 
-        if v['variety'] not in varieties:
-            v['variety'] = 0
+        if v.__getattribute__('variety') not in varieties:
+            v.__setattr__('variety', 0)
         else:
-            variety_index = varieties.index(v['variety']) + 1
+            variety_index = varieties.index(v.__getattribute__('variety')) + 1
             value_variety = (1 / len_varieties) * variety_index
-            v['variety'] = value_variety
+            v.__setattr__('variety', value_variety)
 
-        if v['winery'] not in wineries:
-            v['winery'] = 0
+        if v.__getattribute__('winery') not in wineries:
+            v.__setattr__('winery',  0)
         else:
-            winery_index = wineries.index(v['winery']) + 1
+            winery_index = wineries.index(v.__getattribute__('winery')) + 1
             value_winery = (1 / len_wineries) * winery_index
-            v['winery'] = value_winery
+            v.__setattr__('winery', value_winery)
 
-        if v['taster_name'] not in taster_names:
-            v['taster_name'] = 0
+        if v.__getattribute__('taster_name') not in taster_names:
+            v.__setattr__('taster_name', 0)
         else:
-            taster_name_index = taster_names.index(v['taster_name']) + 1
+            taster_name_index = taster_names.index(v.__getattribute__('taster_name')) + 1
             value_taster_name = (1 / len_taster_names) * taster_name_index
-            v['taster_name'] = value_taster_name
+            v.__setattr__('taster_name', value_taster_name)
 
-        if v['title'] not in titles:
-            v['title'] = 0
+        if v.__getattribute__('title') not in titles:
+            v.__setattr__('title',  0)
         else:
-            title_index = titles.index(v['title']) + 1
+            title_index = titles.index(v.__getattribute__('title')) + 1
             value_title = (1 / len_title) * title_index
-            v['title'] =  value_title
+            v.__setattr__('title', value_title)
 
-        if (int(v['points']) < 85):
-            v['points'] = 0
-        elif (int(v['points']) < 90):
-            v['points'] = 0.33
-        elif (int(v['points']) < 95):
-            v['points'] = 0.67
+        if (int(v.__getattribute__('points')) < 85):
+            v.__setattr__('points', 0)
+        elif (int(v.__getattribute__('points')) < 90):
+            v.__setattr__('points', 0.33)
+        elif (int(v.__getattribute__('points')) < 95):
+            v.__setattr__('points', 0.67)
         else:
-            v['points'] = 1
+            v.__setattr__('points', 1)
+
 
 
 def naiveBayes():
@@ -191,24 +192,34 @@ def naiveBayes():
         "taster_name",
         "title"
     ]
+    print(str(type(treningSet)))
+    treningFrame = pd.DataFrame(np.array(treningSet).reshape(len(treningSet), 10), columns=['country', 'description', 'points', 'price', 'province', 'taster_name', 'title', 'variety', 'winery', 'descNum'])
+    testFrame = pd.DataFrame(np.array(testSet).reshape(len(testSet), 10), columns=['country', 'description', 'points', 'price', 'province', 'taster_name', 'title', 'variety', 'winery', 'descNum'])
     gnb.fit(
-        treningSet[used_features].values,
-        treningSet["points"]
+        treningFrame[used_features].values,
+        treningFrame['points']
     )
     y_pred = gnb.predict(testSet[used_features])
     print("Number of mislabeled points out of a total {} points : {}, performance {:05.2f}%"
         .format(
-        testSet.shape[0],
-        (testSet["points"] != y_pred).sum(),
-        100 * (1 - (testSet["points"] != y_pred).sum() / testSet.shape[0])
+        testFrame.shape[0],
+        (testFrame["points"] != y_pred).sum(),
+        100 * (1 - (testFrame["points"] != y_pred).sum() / testFrame.shape[0])
     ))
 
 loadTreningSet()
 print("zavrsio1")
 processTreningSet()
 print("zavrsio2")
-loadTestSet()
+#loadTestSet()
 print("zavrsio3")
-processTestSet()
+#processTestSet()
+print("test: "+str(len(testSet)))
+print("trening: "+str(len(treningSet)))
+with open('treningCSV.csv', 'w',) as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['country', 'description', 'points', 'price', 'province', 'taster_name', 'title', 'variety', 'winery', 'descNum'])
+    for d in treningSet:
+        writer.writerow([d.__getattribute__('country'), d.__getattribute__('description'), d.__getattribute__('points'), d.__getattribute__('price'), d.__getattribute__('province'), d.__getattribute__('taster_name'), d.__getattribute__('title'), d.__getattribute__('variety'), d.__getattribute__('winery'), d.__getattribute__('descNum')])
 print("zavrsio4")
 naiveBayes()
